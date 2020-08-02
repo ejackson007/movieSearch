f2ef8e680689348a4d221c27014a4e28345f37e0
Evan Jackson <evan@Evans-iMac.lan>
2020-08-01 22:53:56 -0500

    added routing

--- movieSearch/home/views.py
+++ movieSearch/home/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    return HttpResponse('<h1>Search Home</h1>')
+
+def about(request):
+    return HttpResponse('<h1>Created by Evan Jackson</h1>')
