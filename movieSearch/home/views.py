e99227ff44917e32d281033abb1e5145b195a5f1
Evan Jackson <evan@Evans-iMac.lan>
2020-08-02 21:58:32 -0500

    added base

--- movieSearch/home/views.py
+++ movieSearch/home/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author' : 'Evan',
+        'title' : "Test Card",
+        'content' : 'First Test',
+        'date' : 'August 2, 2020'
+    },
+    {
+        'author' : 'Not Evan',
+        'title' : "Test 2 Card",
+        'content' : 'Second Test',
+        'date' : 'August 2, 2020'
+    }
+]
+
 # Create your views here.
 def home(request):
-    return render(request, 'home/home.html')
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'home/home.html', context)
 
 def about(request):
-    return render(request, 'home/about.html')
+    return render(request, 'home/about.html', {'title' : 'about'})
