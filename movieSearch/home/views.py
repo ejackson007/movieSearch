9ffbda120c33c47c4308c06dbe17c27156d09fd2
Evan Jackson <evan@Evans-iMac.lan>
2020-08-02 21:38:38 -0500

    added templates

--- movieSearch/home/views.py
+++ movieSearch/home/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Search Home</h1>')
+    return render(request, 'home/home.html')
 
 def about(request):
-    return HttpResponse('<h1>Created by Evan Jackson</h1>')
+    return render(request, 'home/about.html')
